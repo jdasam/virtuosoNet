ae29a1d146e3ae1e62a5cf7529f676f59854a844
Dasaem Jeong <jdasam@kaist.ac.kr>
2019-05-17 17:35:00 +0900

    move extract_note to mxp

--- xml_matching.py
+++ xml_matching.py
@@ -32,247 +32,6 @@ DYN_EMB_TAB = dir_enc.define_dyanmic_embedding_table()
 TEM_EMB_TAB = dir_enc.define_tempo_embedding_table()
 
 
-def extract_notes(xml_doc, melody_only=False, grace_note=False):
-    notes = []
-    previous_grace_notes = []
-    rests = []
-    num_parts = len(xml_doc.parts)
-    for instrument_index in range(num_parts):
-        part = xml_doc.parts[instrument_index]
-        measure_number = 1
-        for measure in part.measures:
-            for note in measure.notes:
-                note.measure_number = measure_number
-                note.voice += instrument_index * 10
-                if melody_only:
-                    if note.voice == 1:
-                        notes, previous_grace_notes, rests = check_note_status_and_append(note, notes, previous_grace_notes, rests, grace_note)
-                else:
-                    notes, previous_grace_notes, rests = check_note_status_and_append(note, notes, previous_grace_notes, rests, grace_note)
-
-            measure_number += 1
-        notes = apply_after_grace_note_to_chord_notes(notes)
-        if melody_only:
-            notes = delete_chord_notes_for_melody(notes)
-        notes = apply_tied_notes(notes)
-        notes.sort(key=lambda x: (x.note_duration.xml_position, x.note_duration.grace_order, -x.pitch[1]))
-        notes = check_overlapped_notes(notes)
-        notes = apply_rest_to_note(notes, rests)
-        notes = omit_trill_notes(notes)
-        notes = extract_and_apply_slurs(notes)
-        notes = rearrange_chord_index(notes)
-
-    return notes
-
-
-def apply_tied_notes(xml_notes):
-    tie_clean_list = []
-    for i in range(len(xml_notes)):
-        if xml_notes[i].note_notations.tied_stop == False:
-            tie_clean_list.append(xml_notes[i])
-        else:
-            for j in reversed(range(len(tie_clean_list))):
-                if tie_clean_list[j].note_notations.tied_start and tie_clean_list[j].pitch[1] == xml_notes[i].pitch[1]:
-                    tie_clean_list[j].note_duration.seconds += xml_notes[i].note_duration.seconds
-                    tie_clean_list[j].note_duration.duration += xml_notes[i].note_duration.duration
-                    tie_clean_list[j].note_duration.midi_ticks += xml_notes[i].note_duration.midi_ticks
-                    if xml_notes[i].note_notations.slurs:
-                        for slur in  xml_notes[i].note_notations.slurs:
-                            tie_clean_list[j].note_notations.slurs.append(slur)
-                    break
-    return tie_clean_list
-
-
-def omit_trill_notes(xml_notes):
-    num_notes = len(xml_notes)
-    omit_index = []
-    trill_sign = []
-    wavy_lines = []
-    for i in range(num_notes):
-        note = xml_notes[i]
-        if not note.is_print_object:
-            omit_index.append(i)
-            if note.accidental:
-                # TODO: handle accidentals in non-print notes
-                if note.accidental == 'natural':
-                    pass
-                elif note.accidental == 'sharp':
-                    pass
-                elif note.accidental == 'flat':
-                    pass
-            if note.note_notations.is_trill:
-                trill = {'xml_pos': note.note_duration.xml_position, 'pitch': note.pitch[1]}
-                trill_sign.append(trill)
-        if note.note_notations.wavy_line:
-            wavy_line = note.note_notations.wavy_line
-            wavy_line.xml_position = note.note_duration.xml_position
-            wavy_line.pitch = note.pitch
-            wavy_lines.append(wavy_line)
-
-        # move trill mark to the highest notes of the onset
-        if note.note_notations.is_trill:
-            notes_in_trill_onset = []
-            current_position = note.note_duration.xml_position
-
-            search_index = i
-            while search_index +1 < num_notes and xml_notes[search_index+1].note_duration.xml_position == current_position:
-                search_index += 1
-                notes_in_trill_onset.append(xml_notes[search_index])
-            search_index = i
-
-            while search_index - 1 >= 0 and xml_notes[search_index-1].note_duration.xml_position == current_position:
-                search_index -= 1
-                notes_in_trill_onset.append(xml_notes[search_index])
-
-            for other_note in notes_in_trill_onset:
-                highest_note = note
-                if other_note.voice == note.voice and other_note.pitch[1] > highest_note.pitch[1] and not other_note.note_duration.is_grace_note:
-                    highest_note.note_notations.is_trill = False
-                    other_note.note_notations.is_trill = True
-
-    wavy_lines = combine_wavy_lines(wavy_lines)
-
-    for index in reversed(omit_index):
-        note = xml_notes[index]
-        xml_notes.remove(note)
-
-    if len(trill_sign) > 0:
-        for trill in trill_sign:
-            for note in xml_notes:
-                if note.note_duration.xml_position == trill['xml_pos'] and abs(note.pitch[1] - trill['pitch']) <4 \
-                        and not note.note_duration.is_grace_note:
-                    note.note_notations.is_trill = True
-                    break
-
-    xml_notes = apply_wavy_lines(xml_notes, wavy_lines)
-
-    return xml_notes
-
-
-def check_note_status_and_append(note, notes, previous_grace_notes, rests, include_grace_note):
-    if note.note_duration.is_grace_note:
-        previous_grace_notes.append(note)
-        if include_grace_note:
-            notes.append(note)
-    elif not note.is_rest:
-        if len(previous_grace_notes) > 0:
-            rest_grc = []
-            added_grc = []
-            grace_order = -1
-            for grc in reversed(previous_grace_notes):
-                if grc.voice == note.voice:
-                    note.note_duration.preceded_by_grace_note = True
-                    grc.note_duration.grace_order = grace_order
-                    grc.following_note = note
-                    if grc.chord_index == 0:
-                        grace_order -= 1
-                    added_grc.append(grc)
-
-                    # notes.append(grc)
-                else:
-                    rest_grc.append(grc)
-            num_added = abs(grace_order) -1
-            for grc in added_grc:
-                # grc.note_duration.grace_order /= num_added
-                grc.note_duration.num_grace = num_added
-                if abs(grc.note_duration.grace_order) == num_added:
-                    grc.note_duration.is_first_grace_note = True
-            previous_grace_notes = rest_grc
-        notes.append(note)
-    else:
-        assert note.is_rest
-        if note.is_print_object:
-            rests.append(note)
-
-    return notes, previous_grace_notes, rests
-
-
-def apply_rest_to_note(xml_notes, rests):
-    xml_positions = [note.note_duration.xml_position for note in xml_notes]
-    # concat continuous rests
-    new_rests = []
-    num_rests = len(rests)
-    for i in range(num_rests):
-        rest = rests[i]
-        j = 1
-        current_end = rest.note_duration.xml_position + rest.note_duration.duration
-        current_voice = rest.voice
-        while i + j < num_rests -1:
-            next_rest = rests[i+j]
-            if next_rest.note_duration.duration == 0:
-                break
-            if next_rest.note_duration.xml_position == current_end and next_rest.voice == current_voice:
-                rest.note_duration.duration += next_rest.note_duration.duration
-                next_rest.note_duration.duration = 0
-                current_end = rest.note_duration.xml_position + rest.note_duration.duration
-                if next_rest.note_notations.is_fermata:
-                    rest.note_notations.is_fermata = True
-            elif next_rest.note_duration.xml_position > current_end:
-                break
-            j += 1
-
-        if not rest.note_duration.duration == 0:
-            new_rests.append(rest)
-
-    rests = new_rests
-
-    for rest in rests:
-        rest_position = rest.note_duration.xml_position
-        closest_note_index = binary_index(xml_positions, rest_position)
-        rest_is_fermata = rest.note_notations.is_fermata
-
-        search_index = 0
-        while closest_note_index - search_index >= 0:
-            prev_note = xml_notes[closest_note_index - search_index]
-            if prev_note.voice == rest.voice:
-                prev_note_end = prev_note.note_duration.xml_position + prev_note.note_duration.duration
-                prev_note_with_rest = prev_note_end + prev_note.following_rest_duration
-                if prev_note_end == rest_position:
-                    prev_note.following_rest_duration = rest.note_duration.duration
-                    if rest_is_fermata:
-                        prev_note.followed_by_fermata_rest = True
-                elif prev_note_end < rest_position:
-                    break
-            # elif prev_note_with_rest == rest_position and prev_note.voice == rest.voice:
-            #     prev_note.following_rest_duration += rest.note_duration.duration
-            search_index += 1
-
-    return xml_notes
-
-
-def apply_after_grace_note_to_chord_notes(notes):
-    for note in notes:
-        if note.note_duration.preceded_by_grace_note:
-            onset = note.note_duration.xml_position
-            voice = note.voice
-            chords = find(lambda x: x.note_duration.xml_position == onset and x.voice == voice and not x.note_duration.is_grace_note, notes)
-            for chd in chords:
-                chd.note_duration.preceded_by_grace_note = True
-    return notes
-
-
-def delete_chord_notes_for_melody(melody_notes):
-    note_onset_positions = list(set(note.note_duration.xml_position for note in melody_notes))
-    note_onset_positions.sort()
-    unique_melody = []
-    for onset in note_onset_positions:
-        notes = find(lambda x: x.note_duration.xml_position == onset, melody_notes)
-        if len(notes) == 1:
-            unique_melody.append(notes[0])
-        else:
-            notes.sort(key=lambda x: x.pitch[1])
-            unique_melody.append(notes[-1])
-
-    return unique_melody
-
-
-def find(f, seq):
-    items_list = []
-    for item in seq:
-        if f(item):
-            items_list.append(item)
-    return items_list
-
 
 class MusicFeature():
     def __init__(self):
@@ -349,7 +108,6 @@ class MusicFeature():
 
 
 def extract_score_features(xml_notes, measure_positions, beats=None, qpm_primo=0, vel_standard=False):
-    features = []
     xml_length = len(xml_notes)
     melody_notes = extract_melody_only_from_notes(xml_notes)
     features = []
@@ -491,11 +249,11 @@ def extract_perform_features(xml_doc, xml_notes, pairs, perf_midi, measure_posit
     velocity_mean = calculate_mean_velocity(pairs)
     total_length_tuple = calculate_total_length(pairs)
     # measure_seocnds = make_midi_measure_seconds(pairs, measure_positions)
-    melody_notes = extract_melody_only_from_notes(xml_notes)
-    melody_onset_positions = list(set(note.note_duration.xml_position for note in melody_notes))
-    melody_onset_positions.sort()
-    if not len(melody_notes) == len(melody_onset_positions):
-        print('length of melody notes and onset positions are different')
+    # melody_notes = extract_melody_only_from_notes(xml_notes)
+    # melody_onset_positions = list(set(note.note_duration.xml_position for note in melody_notes))
+    # melody_onset_positions.sort()
+    # if not len(melody_notes) == len(melody_onset_positions):
+    #     print('length of melody notes and onset positions are different')
     beats = cal_beat_positions_of_piece(xml_doc)
     accidentals_in_words = extract_accidental(xml_doc)
     score_features = extract_score_features(xml_notes, measure_positions, beats=beats)
@@ -694,32 +452,6 @@ def calculate_mean_velocity(pairs):
             length += 1
 
     return sum/float(length)
-
-
-def rearrange_chord_index(xml_notes):
-    # assert all(xml_notes[i].pitch[1] >= xml_notes[i + 1].pitch[1] for i in range(len(xml_notes) - 1)
-    #            if xml_notes[i].note_duration.xml_position ==xml_notes[i+1].note_duration.xml_position)
-
-    previous_position = [-1]
-    max_chord_index = [0]
-    for note in xml_notes:
-        voice = note.voice -1
-        while voice >= len(previous_position):
-            previous_position.append(-1)
-            max_chord_index.append(0)
-        if note.note_duration.is_grace_note:
-            continue
-        if note.staff ==1:
-            if note.note_duration.xml_position > previous_position[voice]:
-                previous_position[voice] = note.note_duration.xml_position
-                max_chord_index[voice] = note.chord_index
-                note.chord_index = 0
-            else:
-                note.chord_index = (max_chord_index[voice] - note.chord_index)
-        else: #note staff ==2
-            pass
-
-    return xml_notes
 
 
 def cal_pitch_interval_and_duration_ratio(xml_notes, index):
@@ -2073,34 +1805,6 @@ def check_pairs(pairs):
     return non_matched
 
 
-def check_overlapped_notes(xml_notes):
-    previous_onset = -1
-    notes_on_onset = []
-    pitches = []
-    for note in xml_notes:
-        if note.note_duration.is_grace_note:
-            continue # does not count grace note, because it can have same pitch on same xml_position
-        if note.note_duration.xml_position > previous_onset:
-            previous_onset = note.note_duration.xml_position
-            pitches = []
-            pitches.append(note.pitch[1])
-            notes_on_onset = []
-            notes_on_onset.append(note)
-        else: #note has same xml_position
-            if note.pitch[1] in pitches: # same pitch with same
-                index_of_same_pitch_note = pitches.index(note.pitch[1])
-                previous_note = notes_on_onset[index_of_same_pitch_note]
-                if previous_note.note_duration.duration > note.note_duration.duration:
-                    note.is_overlapped = True
-                else:
-                    previous_note.is_overlapped = True
-            else:
-                pitches.append(note.pitch[1])
-                notes_on_onset.append(note)
-
-    return xml_notes
-
-
 def read_xml_to_array(path_name, means, stds, start_tempo, composer_name, vel_standard):
     xml_name = path_name + 'musicxml_cleaned.musicxml'
 
@@ -2706,56 +2410,6 @@ def extract_accidental(xml_doc):
     return directions
 
 
-def combine_wavy_lines(wavy_lines):
-    num_wavy = len(wavy_lines)
-    for i in reversed(range(num_wavy)):
-        wavy = wavy_lines[i]
-        if wavy.type == 'stop':
-            deleted = False
-            for j in range(1, i+1):
-                prev_wavy = wavy_lines[i - j]
-                if prev_wavy.type == 'start' and prev_wavy.number == wavy.number:
-                    prev_wavy.end_xml_position = wavy.xml_position
-                    wavy_lines.remove(wavy)
-                    deleted = True
-                    break
-            if not deleted:
-                wavy_lines.remove(wavy)
-    num_wavy = len(wavy_lines)
-    for i in reversed(range(num_wavy)):
-        wavy = wavy_lines[i]
-        if wavy.type == 'start' and wavy.end_xml_position == 0:
-            wavy_lines.remove(wavy)
-    return wavy_lines
-
-
-def apply_wavy_lines(xml_notes, wavy_lines):
-    xml_positions = [x.note_duration.xml_position for x in xml_notes]
-    num_notes = len(xml_notes)
-    omit_indices = []
-    for wavy in wavy_lines:
-        index = binary_index(xml_positions, wavy.xml_position)
-        while abs(xml_notes[index].pitch[1] - wavy.pitch[1]) > 3 and index > 0 \
-                and xml_notes[index-1].note_duration.xml_position == xml_notes[index].note_duration.xml_position:
-            index -= 1
-        note = xml_notes[index]
-        wavy_duration = wavy.end_xml_position - wavy.xml_position
-        note.note_duration.duration = wavy_duration
-        trill_pitch = note.pitch[1]
-        next_idx = index+1
-        while next_idx < num_notes and xml_notes[next_idx].note_duration.xml_position < wavy.end_xml_position:
-            if xml_notes[next_idx].pitch[1] == trill_pitch:
-                omit_indices.append(next_idx)
-            next_idx += 1
-
-    omit_indices.sort()
-    if len(omit_indices)> 0:
-        for idx in reversed(omit_indices):
-            del xml_notes[idx]
-
-    return xml_notes
-
-
 def get_measure_accidentals(xml_notes, index):
     accs = ['bb', 'b', '♮', '#', 'x']
     note = xml_notes[index]
@@ -2858,50 +2512,6 @@ def cal_mean_velocity_by_dynamics_marking(features):
     return mean_piano_vel, mean_piano_marking, mean_forte_vel, mean_forte_marking
 
 
-def extract_and_apply_slurs(xml_notes):
-    resolved_slurs = []
-    unresolved_slurs = []
-    slur_index = 0
-    for note in xml_notes:
-        slurs = note.note_notations.slurs
-        if slurs:
-            for slur in reversed(slurs):
-                slur.xml_position = note.note_duration.xml_position
-                slur.voice = note.voice
-                type = slur.type
-                if type == 'start':
-                    slur.index = slur_index
-                    unresolved_slurs.append(slur)
-                    slur_index += 1
-                    note.note_notations.is_slur_start = True
-                elif type == 'stop':
-                    note.note_notations.is_slur_stop = True
-                    for prev_slur in unresolved_slurs:
-                        if prev_slur.number == slur.number and prev_slur.voice == slur.voice:
-                            prev_slur.end_xml_position = slur.xml_position
-                            resolved_slurs.append(prev_slur)
-                            unresolved_slurs.remove(prev_slur)
-                            note.note_notations.slurs.remove(slur)
-                            note.note_notations.slurs.append(prev_slur)
-                            break
-
-    for note in xml_notes:
-        slurs = note.note_notations.slurs
-        note_position = note.note_duration.xml_position
-        if not slurs:
-            for prev_slur in resolved_slurs:
-                if prev_slur.voice == note.voice and prev_slur.xml_position <= note_position <= prev_slur.end_xml_position:
-                    note.note_notations.slurs.append(prev_slur)
-                    if prev_slur.xml_position == note_position:
-                        note.note_notations.is_slur_start = True
-                    elif prev_slur.end_xml_position == note_position:
-                        note.note_notations.is_slur_stop = True
-                    else:
-                        note.note_notations.is_slur_continue = True
-
-    return xml_notes
-
-
 def pedal_sigmoid(pedal_value, k=8):
     sigmoid_pedal = 127 / (1 + math.exp(-(pedal_value-64)/k))
     return int(sigmoid_pedal)
@@ -2927,7 +2537,7 @@ def read_score_perform_pair(path, perf_name, composer_name, means, stds):
         score_midi_name = path + 'midi.mid'
 
     xml_object = MusicXMLDocument(xml_name)
-    xml_notes = extract_notes(xml_object, melody_only=False, grace_note=True)
+    xml_notes = xml_object.get_notes(melody_only=False, grace_note=True)
     directions, time_signatures = extract_directions(xml_object)
     xml_notes = apply_directions_to_notes(xml_notes, directions, time_signatures)
 
