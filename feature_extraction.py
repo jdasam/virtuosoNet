d0f33df4ff26a054d8f806e9f2caaa7729f69dbc
yoojin <lj2141301@gmail.com>
2020-01-14 15:34:27 +0900

    move codes to feature_utils.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,11 @@
 from . import xml_direction_encoding as dir_enc
-from . import xml_utils, utils
-
+from . import xml_utils, utils, feature_utils
+class NoteLocation:
+    def __init__(self, beat, measure, voice, section):
+        self.beat = beat
+        self.measure = measure
+        self.voice = voice
+        self.section = section
 
 class ScoreExtractor:
     def __init__(self, feature_keys):
@@ -11,7 +16,7 @@ class ScoreExtractor:
         self.beat_positions = piece_data.beat_positions
         self.measure_positions = piece_data.measure_positions
     '''
-    
+
     def extract_and_update_score_features(self, piece_data):
         def _get_beat_position(piece_data):
             beat_positions = []
@@ -34,7 +39,7 @@ class ScoreExtractor:
             beat_positions = _get_beat_position(piece_data)
             beat_importances = []
             for i, note in enumerate(piece_data.xml_notes):
-                importance = cal_beat_importance(
+                importance = feature_utils.cal_beat_importance(
                     beat_positions[i], note.tempo.time_numerator)
                 beat_importances.append(importance)
             return beat_importances
@@ -70,6 +75,9 @@ class ScoreExtractor:
         DYN_EMB_TAB = dir_enc.define_dyanmic_embedding_table()
         TEM_EMB_TAB = dir_enc.define_tempo_embedding_table()
 
+        '''
+        # TODO: unused.. separate from here?
+        '''
         piece_data.qpm_primo = piece_data.xml_notes[0].state_fixed.qpm
         tempo_primo_word = dir_enc.direction_words_flatten(
             piece_data.xml_notes[0].tempo)
@@ -86,7 +94,7 @@ class ScoreExtractor:
         features['note_location'] = self.get_note_location(piece_data)
         features['midi_pitch'] = [note.pitch[1]
                                   for note in piece_data.xml_notes]
-        features['pitch'] = [pitch_into_vector(
+        features['pitch'] = [feature_utils.pitch_into_vector(
             note.pitch[1]) for note in piece_data.xml_notes]
         features['duration'] = [note.note_duration.duration / note.state_fixed.divisions
                                 for note in piece_data.xml_notes]
@@ -102,13 +110,13 @@ class ScoreExtractor:
             int(note.note_duration.is_grace_note) for note in piece_data.xml_notes]
         features['preceded_by_grace_note'] = [
             int(note.note_duration.preceded_by_grace_note) for note in piece_data.xml_notes]
-        features['time_sig_vec'] = [time_signature_to_vector(
+        features['time_sig_vec'] = [feature_utils.time_signature_to_vector(
             note.tempo.time_signature) for note in piece_data.xml_notes]
         features['following_rest'] = [note.following_rest_duration /
                                       note.state_fixed.divisions for note in piece_data.xml_notes]
         features['followed_by_fermata_rest'] = [
             int(note.followed_by_fermata_rest) for note in piece_data.xml_notes]
-        features['notation'] = [note_notation_to_vector(
+        features['notation'] = [feature_utils.note_notation_to_vector(
             note) for note in piece_data.xml_notes]
 
         # TODO: better to save it as dict
@@ -151,7 +159,7 @@ class ScoreExtractor:
             features.append(feature)
 
         '''
-        features = make_index_continuous(features, score=False)
+        features = feature_utils.make_index_continuous(features, score=False)
 
         return features
 
@@ -179,138 +187,3 @@ class ScoreExtractor:
                              voice=note.voice,
                              section=utils.binary_index(piece_data.section_positions, note.note_duration.xml_position)))
         return locations
-
-def cal_beat_importance(beat_position, numerator):
-    # beat_position : [0-1), note's relative position in measure
-    if beat_position == 0:
-        beat_importance = 4
-    elif beat_position == 0.5 and numerator in [2, 4, 6, 12]:
-        beat_importance = 3
-    elif abs(beat_position - (1/3)) < 0.001 and numerator in [3, 9]:
-        beat_importance = 2
-    elif (beat_position * 4) % 1 == 0 and numerator in [2, 4]:
-        beat_importance = 1
-    elif (beat_position * 5) % 1 == 0 and numerator in [5]:
-        beat_importance = 2
-    elif (beat_position * 6) % 1 == 0 and numerator in [3, 6, 12]:
-        beat_importance = 1
-    elif (beat_position * 8) % 1 == 0 and numerator in [2, 4]:
-        beat_importance = 0.5
-    elif (beat_position * 9) % 1 == 0 and numerator in [9]:
-        beat_importance = 1
-    elif (beat_position * 12) % 1 == 0 and numerator in [3, 6, 12]:
-        beat_importance = 0.5
-    elif numerator == 7:
-        if abs((beat_position * 7) - 2) < 0.001:
-            beat_importance = 2
-        elif abs((beat_position * 5) - 2) < 0.001:
-            beat_importance = 2
-        else:
-            beat_importance = 0
-    else:
-        beat_importance = 0
-    return beat_importance
-
-
-def pitch_into_vector(pitch):
-    # TODO: should be located in general file. maybe utils?
-    pitch_vec = [0] * 13  # octave + pitch class
-    octave = (pitch // 12) - 1
-    octave = (octave - 4) / 4  # normalization
-    pitch_class = pitch % 12
-
-    pitch_vec[0] = octave
-    pitch_vec[pitch_class+1] = 1
-
-    return pitch_vec
-
-def time_signature_to_vector(time_signature):
-    numerator = time_signature.numerator
-    denominator = time_signature.denominator
-
-    denominator_list = [2, 4, 8, 16]
-    numerator_vec = [0] * 5
-    denominator_vec = [0] * 4
-
-    if denominator == 32:
-        denominator_vec[-1] = 1
-    else:
-        denominator_type = denominator_list.index(denominator)
-        denominator_vec[denominator_type] = 1
-
-    if numerator == 2:
-        numerator_vec[0] = 1
-    elif numerator == 3:
-        numerator_vec[1] = 1
-    elif numerator == 4:
-        numerator_vec[0] = 1
-        numerator_vec[2] = 1
-    elif numerator == 6:
-        numerator_vec[0] = 1
-        numerator_vec[3] = 1
-    elif numerator == 9:
-        numerator_vec[1] = 1
-        numerator_vec[3] = 1
-    elif numerator == 12:
-        numerator_vec[0] = 1
-        numerator_vec[2] = 1
-        numerator_vec[3] = 1
-    elif numerator == 24:
-        numerator_vec[0] = 1
-        numerator_vec[2] = 1
-        numerator_vec[3] = 1
-    else:
-        print('Unclassified numerator: ', numerator)
-        numerator_vec[4] = 1
-
-    return numerator_vec + denominator_vec
-
-
-def note_notation_to_vector(note):
-    # trill, tenuto, accent, staccato, fermata
-    keywords = ['is_trill', 'is_tenuto', 'is_accent', 'is_staccato',
-                'is_fermata', 'is_arpeggiate', 'is_strong_accent', 'is_cue', 'is_slash']
-    # keywords = ['is_trill', 'is_tenuto', 'is_accent', 'is_staccato', 'is_fermata']
-
-    notation_vec = [0] * len(keywords)
-
-    for i in range(len(keywords)):
-        key = keywords[i]
-        if getattr(note.note_notations, key) == True:
-            notation_vec[i] = 1
-
-    return notation_vec
-
-
-def make_index_continuous(features, score=False):
-    # TODO: what's the role?
-    prev_beat = 0
-    prev_measure = 0
-
-    beat_compensate = 0
-    measure_compensate = 0
-
-    for feat in features:
-        if feat.qpm is not None or score:
-            if feat.note_location.beat - prev_beat > 1:
-                beat_compensate -= (feat.note_location.beat - prev_beat) - 1
-            if feat.note_location.measure - prev_measure > 1:
-                measure_compensate -= (feat.note_location.measure -
-                                       prev_measure) - 1
-
-            prev_beat = feat.note_location.beat
-            prev_measure = feat.note_location.measure
-
-            feat.note_location.beat += beat_compensate
-            feat.note_location.measure += measure_compensate
-        else:
-            continue
-    return features
-
-
-class NoteLocation:
-    def __init__(self, beat, measure, voice, section):
-        self.beat = beat
-        self.measure = measure
-        self.voice = voice
-        self.section = section
