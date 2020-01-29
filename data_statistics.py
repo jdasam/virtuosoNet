58afee42c32f5fb66e89b23af0624c6e30dd16f4
Dasaem Jeong <jdasam@kaist.ac.kr>
2020-01-29 22:12:11 +0900

    WIP refactoring initialization

--- data_statistics.py
+++ data_statistics.py
@@ -65,6 +65,90 @@ def calculate_ratio_of_feature_true(indexes):
             ))
 
 
+def cal_correlation_of_pairs_in_folder(path):
+    features_in_folder = load_pairs_from_folder(path)
+    if features_in_folder is None:
+        return None
+    num_performance = len(features_in_folder)
+    if num_performance < 3:
+        print('Error: There are only {} performances in the folder'.format(num_performance))
+        return None
+
+    num_notes = len(features_in_folder[0]['features'])
+    beat_numbers = [x.note_location.beat for x in features_in_folder[0]['features']]
+    slice_indexes = make_slicing_indexes_by_beat(beat_numbers, 30, overlap=True)
+    correlation_result_total = []
+
+    for slc_idx in slice_indexes:
+        correlation_result = CorrelationResult(path, slc_idx)
+        correlation_result.num_performance = num_performance
+
+        for features in features_in_folder:
+            sliced_features = features['features'][slc_idx[0]:slc_idx[1]]
+            tempos, dynamics = perf_worm.cal_tempo_and_velocity_by_beat(sliced_features)
+            correlation_result.tempo_features.append(tempos)
+            correlation_result.dynamic_features.append(dynamics)
+        correlation_result._cal_correlation_of_features()
+
+        min_tempo_r, min_vel_r = correlation_result.cal_minimum()
+        if min_tempo_r > 0.7:
+            save_name = 'test_plot/' + path.replace('chopin_cleaned/', '').replace('/', '_', 10) + '_note{}-{}.png'.format(slc_idx[0], slc_idx[1])
+            perf_worm.plot_normalized_feature(correlation_result.tempo_features, save_name)
+            correlation_result_total.append(correlation_result)
+
+    return correlation_result_total
+
+
+class CorrelationResult:
+    def __init__(self, path=None, slc_idx=None):
+        self.tempo_r = []
+        self.dynamic_r = []
+        self.path_name = path
+        self.slice_index = slc_idx
+        self.tempo_features = []
+        self.dynamic_features = []
+        self.num_performance = 0
+
+    def _append_result(self, tempo_r, velocity_r):
+        self.tempo_r.append(tempo_r)
+        self.dynamic_r.append(velocity_r)
+
+    def cal_median(self):
+        return np.median(self.tempo_r), np.median(self.dynamic_r)
+
+    def cal_minimum(self):
+        return min(self.tempo_r), min(self.dynamic_r)
+
+    def cal_maximum(self):
+        return max(self.tempo_r), max(self.dynamic_r)
+
+    def _cal_correlation_of_features(self):
+        for i in range(self.num_performance-1):
+            for j in range(i+1, self.num_performance):
+                tempo_r, _ = cal_correlation(self.tempo_features[i], self.tempo_features[j])
+                dynamic_r, _ = cal_correlation(self.dynamic_features[i], self.dynamic_features[j])
+                self._append_result(tempo_r, dynamic_r)
+
+
+    def __str__(self):
+        if len(self.tempo_r) > 0:
+            tempo_r_median, dynamic_r_median = self.cal_median()
+            tempo_r_min, dynamic_r_min = self.cal_minimum()
+            tempo_r_max, dynamic_r_max = self.cal_maximum()
+        else:
+            return 'No correlation'
+
+        return 'Piece: {}, Note index : {}, Tempo Med: {:.4f}, Min: {:.4f}, Max: {:.4f} - Dynamic Med: {:.4f}, Min: {:.4f}, Max: {:.4f}'\
+            .format(self.path_name, self.slice_index, tempo_r_median, tempo_r_min, tempo_r_max, dynamic_r_median, dynamic_r_min, dynamic_r_max)
+
+
+def cal_correlation(feat_a, feat_b):
+    if len(feat_a) != len(feat_b):
+        print('Error: length of two tempos are different, length a: {}, length b: {}'.format(len(feat_a), len(feat_b)))
+        return None
+
+    return scipy.stats.pearsonr(feat_a, feat_b)
+
 plot_and_save_figures()
 # target_indexes = [(0,12), (0,13), (0,-11), (0,15)]
 # calculate_ratio_of_feature_true(target_indexes)
--- run_code.py
+++ run_code.py
@@ -1,7 +1,7 @@
 import style_analysis
 import pyScoreParser.xml_matching as xml_matching
 import pyScoreParser.midi_utils.midi_utils as midi_utils
-import pyScoreParser.xml_matching as xml_matching
+import pyScoreParser.data_class as data_class
 import pickle
 import _pickle as cPickle
 import numpy as np
@@ -23,25 +23,29 @@ import numpy as np
 #
 # xml_matching.save_midi_notes_as_piano_midi(midi_notes, midi_pedals, output_name, bool_pedal=False, disklavier=False)
 
-# data_set = xml_matching.DataSet('MIDI_XML_sarah/')
-# data_set._load_all_performances()
-# data_set._extract_all_features()
+# data_set = data_class.DataSet('MIDI_XML_sarah/')
+# data_set.load_all_performances()
+# data_set.extract_all_features()
 # data_set.save_dataset()
 
 with open('data_set.dat', "rb") as f:
     u = cPickle.Unpickler(f)
     data_set = u.load()
-#
-#
-target_features = ['attack_deviation', 'abs_deviation', 'tempo_fluctuation', 'left_hand_velocity', 'right_hand_velocity', 'velocity']
+
+
+target_features = ['articulation', 'attack_deviation', 'abs_deviation', 'tempo_fluctuation', 'left_hand_velocity', 'right_hand_velocity',
+                   'velocity', 'right_hand_attack_deviation', 'left_hand_attack_deviation', 'pedal_at_start', 'pedal_at_end', 'pedal_refresh', 'pedal_cut']
 data_set._divide_by_tag(['amateur', 'professional'])
 data_set._sort_performances()
 data_set.default_performances = data_set.flattened_performs_by_tag
-data_set._extract_selected_features(target_features)
+data_set.extract_selected_features(target_features)
 features = data_set.features_to_list(target_features)
 features = data_set.get_average_by_perform(features)
-data_set.save_features_as_csv(features)
+data_set.save_features_as_csv(features, target_features, path='features_by_piecewise_average.csv')
 
 
 measure_level_features = data_set.get_average_feature_by_measure(target_features)
 data_set.save_features_by_features_as_csv(measure_level_features, target_features)
+
+features = data_set.features_to_list(target_features)
+data_set.save_features_by_features_as_csv(features, target_features, path='note.csv')
