13e35e8ca4b9d2e7d362cd610b424def993b6c5c
Dasaem Jeong <jdasam@kaist.ac.kr>
2020-02-12 09:11:52 +0900

    Test code update

--- run_code.py
+++ run_code.py
@@ -5,6 +5,8 @@ import pyScoreParser.data_class as data_class
 import pickle
 import _pickle as cPickle
 import numpy as np
+import pyScoreParser.data_for_training as dft
+import pyScoreParser.feature_extraction as feat_ext
 
 
 # style_analysis.load_tsne_and_plot("style_tsne_z.dat")
@@ -23,29 +25,65 @@ import numpy as np
 #
 # xml_matching.save_midi_notes_as_piano_midi(midi_notes, midi_pedals, output_name, bool_pedal=False, disklavier=False)
 
-# data_set = data_class.DataSet('MIDI_XML_sarah/')
+# data_set = data_class.DataSet('EmotionData/', 'name')
 # data_set.load_all_performances()
-# data_set.extract_all_features()
-# data_set.save_dataset()
+# data_set.save_dataset('EmotionData.dat')
 
-with open('data_set.dat', "rb") as f:
+
+# test_piece = data_class.PieceData('EmotionData/Liszt.dante-sonata_s161_no7_.mm_35-54.musicxml', 'name')
+# print(test_piece)
+
+
+# with open('SarahData.dat', "rb") as f:
+#     u = cPickle.Unpickler(f)
+#     data_set = u.load()
+#
+# # data_set.extract_all_features()
+# # data_set.save_dataset('SarahData.dat')
+# # for perf in data_set.performances:
+# #     perf.perform_features = {}
+#
+# # target_features = ['articulation']
+# # data_set._divide_by_tag(['amateur', 'professional'])
+# # data_set._sort_performances()
+# # data_set.default_performances = data_set.flattened_performs_by_tag
+# # data_set.extract_selected_features(target_features)
+#
+# values = dft.get_feature_from_entire_dataset(data_set, ['midi_pitch', 'pitch', 'duration'], ['beat_tempo', 'velocity'])
+# values = dft.normalize_feature(values, ['midi_pitch', 'duration', 'velocity'])
+#
+# pair_data = dft.PairDataset(data_set)
+# pair_data.update_dataset_split_type()
+#
+# print(values)
+
+
+
+# data_set = data_class.DataSet('pyScoreParser/chopin_cleaned/Haydn/', 'folder')
+# data_set.load_all_performances()
+# # score_extractor = feat_ext.ScoreExtractor(['composer_vec'])
+# for piece in data_set.pieces:
+#     piece.meta.composer = 'Haydn'
+# #     piece.score_features['composer_vec'] = score_extractor.get_composer_vec(piece)
+# data_set.extract_all_features()
+# data_set.save_dataset('HaydnTest.dat')
+
+with open('HaydnTest.dat', "rb") as f:
     u = cPickle.Unpickler(f)
     data_set = u.load()
 
-
-target_features = ['articulation', 'attack_deviation', 'abs_deviation', 'tempo_fluctuation', 'left_hand_velocity', 'right_hand_velocity',
-                   'velocity', 'right_hand_attack_deviation', 'left_hand_attack_deviation', 'pedal_at_start', 'pedal_at_end', 'pedal_refresh', 'pedal_cut']
-data_set._divide_by_tag(['amateur', 'professional'])
-data_set._sort_performances()
-data_set.default_performances = data_set.flattened_performs_by_tag
-data_set.extract_selected_features(target_features)
-features = data_set.features_to_list(target_features)
-features = data_set.get_average_by_perform(features)
-data_set.save_features_as_csv(features, target_features, path='features_by_piecewise_average.csv')
+# perform_extractor = feat_ext.PerformExtractor(['beat_dynamics', 'measure_dynamics'])
+# for piece in data_set.pieces:
+#     for performance in piece.performances:
+#         performance.perform_features = perform_extractor.extract_perform_features(piece, performance)
+# data_set.save_dataset('HaydnTest.dat')
 
 
-measure_level_features = data_set.get_average_feature_by_measure(target_features)
-data_set.save_features_by_features_as_csv(measure_level_features, target_features)
+#
+#
 
-features = data_set.features_to_list(target_features)
-data_set.save_features_by_features_as_csv(features, target_features, path='note.csv')
+pair_data = dft.PairDataset(data_set)
+pair_data.update_dataset_split_type()
+pair_data.update_mean_stds_of_entire_dataset()
+pair_data.save_features_for_virtuosoNet('HaydnTestFeature')
+
