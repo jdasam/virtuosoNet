8d457c0c03b21e813433127569bfea4f38285411
jdasam <jdasam@kaist.ac.kr>
2020-08-16 18:49:04 +0900

    wip refactoring

--- virtuoso/inference.py
+++ virtuoso/inference.py
@@ -3,22 +3,25 @@ import argparse
 import torch
 
 from .constants import *
+from ..pyScoreParser.data_class import ScoreData
+from ..pyScoreParser.feature_extraction import ScoreExtractor
+from pathlib import Path
 
-def scale_model_prediction_to_original(prediction, MEANS, STDS):
-    for i in range(len(STDS)):
-        for j in range(len(STDS[i])):
-            if STDS[i][j] < 1e-4:
-                STDS[i][j] = 1
+def scale_model_prediction_to_original(prediction, means, stds, loss_type='MSE'):
+    for i in range(len(stds)):
+        for j in range(len(stds[i])):
+            if stds[i][j] < 1e-4:
+                stds[i][j] = 1
     prediction = np.squeeze(np.asarray(prediction.cpu()))
     num_notes = len(prediction)
-    if LOSS_TYPE == 'MSE':
+    if loss_type == 'MSE':
         for i in range(11):
-            prediction[:, i] *= STDS[1][i]
-            prediction[:, i] += MEANS[1][i]
+            prediction[:, i] *= stds[1][i]
+            prediction[:, i] += means[1][i]
         for i in range(11, 15):
-            prediction[:, i] *= STDS[1][i+4]
-            prediction[:, i] += MEANS[1][i+4]
-    elif LOSS_TYPE == 'CE':
+            prediction[:, i] *= stds[1][i+4]
+            prediction[:, i] += means[1][i+4]
+    elif loss_type == 'CE':
         prediction_in_value = np.zeros((num_notes, 16))
         for i in range(num_notes):
             bin_range_start = 0
@@ -36,13 +39,21 @@ def scale_model_prediction_to_original(prediction, MEANS, STDS):
     return prediction
 
 
-def load_file_and_generate_performance(model, path_name, args, hier_model=None, return_features=False):
+def load_file_and_generate_performance(model, path_name, args, trill_model, hier_model=None, return_features=False):
     composer=args.composer
     z=args.latent
-    start_tempo=args.startTempo 
-    vel_pair = (int(args.velocity.split(',')[0]), int(
-        args.velocity.split(',')[1]))
-    test_x, xml_notes, xml_doc, edges, note_locations = xml_matching.read_xml_to_array(path_name, MEANS, STDS,
+    start_tempo=args.startTempo
+    path_name = Path(path_name)
+    if path_name.suffix not in ('xml', 'musicxml'):
+        path_name = path_name / 'xml.xml'
+        if not path_name.exists():
+            path_name = path_name.parent / 'musicxml_cleaned.musicxml'
+
+    score_features = model.score_feature_keys
+    score_data = ScoreData(path_name)
+    feature_extractor = ScoreExtractor()
+    test_x = feature_extractor.extract_score_features(score_data)
+    test_x, xml_notes, xml_doc, edges, note_locations = xml_matching.read_xml_to_array(path_name, means, stds,
                                                                                        start_tempo, composer,
                                                                                        vel_pair)
     batch_x = torch.Tensor(test_x)
@@ -58,7 +69,7 @@ def load_file_and_generate_performance(model, path_name, args, hier_model=None, 
         initial_z = 'zero'
 
     if IN_HIER:
-        batch_x = batch_x.to(DEVICE).view(1, -1, HIER_MODEL.input_size)
+        batch_x = batch_x.to(DEVICE).view(1, -1, hier_model.input_size)
         graph = edges_to_matrix(edges, batch_x.shape[1])
         MODEL.is_teacher_force = False
         if type(initial_z) is list:
@@ -68,32 +79,32 @@ def load_file_and_generate_performance(model, path_name, args, hier_model=None, 
             # hier_z = [z] * HIER_MODEL_PARAM.encoder.size
             hier_z = 'zero'
             final_z = initial_z
-        hier_input_y = torch.zeros(1, num_notes, HIER_MODEL.output_size)
+        hier_input_y = torch.zeros(1, num_notes, hier_model.output_size)
         hier_output, _ = run_model_in_steps(
-            batch_x, hier_input_y, graph, note_locations, initial_z=hier_z, model=HIER_MODEL)
+            batch_x, hier_input_y, graph, note_locations, initial_z=hier_z, model=hier_model)
         if 'measure' in args.hierCode:
             hierarchy_numbers = [x.measure for x in note_locations]
         else:
             hierarchy_numbers = [x.section for x in note_locations]
-        hier_output_spanned = HIER_MODEL.span_beat_to_note_num(
+        hier_output_spanned = hier_model.span_beat_to_note_num(
             hier_output, hierarchy_numbers, len(test_x), 0)
         combined_x = torch.cat((batch_x, hier_output_spanned), 2)
         prediction, _ = run_model_in_steps(
-            combined_x, input_y, graph, note_locations, initial_z=final_z, model=MODEL)
+            combined_x, input_y, graph, note_locations, initial_z=final_z, model=model)
     else:
         if type(initial_z) is list:
             initial_z = initial_z[0]
         batch_x = batch_x.to(DEVICE).view(1, -1, NUM_INPUT)
         graph = edges_to_matrix(edges, batch_x.shape[1])
         prediction, _ = run_model_in_steps(
-            batch_x, input_y, graph, note_locations, initial_z=initial_z, model=MODEL)
+            batch_x, input_y, graph, note_locations, initial_z=initial_z, model=model)
 
     trill_batch_x = torch.cat((batch_x, prediction), 2)
     trill_prediction, _ = run_model_in_steps(trill_batch_x, torch.zeros(
-        1, num_notes, cons.num_trill_param), graph, note_locations, model=TRILL_MODEL)
+        1, num_notes, cons.num_trill_param), graph, note_locations, model=trill_model)
 
     prediction = torch.cat((prediction, trill_prediction), 2)
-    prediction = scale_model_prediction_to_original(prediction, MEANS, STDS)
+    prediction = scale_model_prediction_to_original(prediction, means, stds)
 
     output_features = xml_matching.model_prediction_to_feature(prediction)
     output_features = xml_matching.add_note_location_to_features(
