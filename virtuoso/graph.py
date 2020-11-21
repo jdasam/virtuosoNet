3efb672647aaec81347d7eec7a3f79bc7748addb
jdasam <dasaem.jeong@sktbrain.com>
2020-11-21 21:31:57 +0900

    make edgs_to_matrix much faster

--- virtuoso/graph.py
+++ virtuoso/graph.py
@@ -8,23 +8,31 @@ N_EDGE_TYPE=0
 def edges_to_matrix(edges, num_notes, graph_keys):
     num_keywords = len(graph_keys)
     matrix = np.zeros((num_keywords * 2, num_notes, num_notes))
+    graph_dict = {key: i for i, key in enumerate(graph_keys) }
 
-    for edg in edges:
-        if edg[2] not in graph_keys:
-            continue
-        edge_type = graph_keys.index(edg[2])
-        matrix[edge_type, edg[0], edg[1]] = 1
-        if edge_type != 0:
-            matrix[edge_type+num_keywords, edg[1], edg[0]] = 1
-        else:
-            matrix[edge_type, edg[1], edg[0]] = 1
+    edg_indices = [(graph_dict[edg[2]], edg[0], edg[1])  
+                for edg in edges
+                if edg[2] in graph_dict]
+    reverse_indices = [ (edg[0]+num_keywords, edg[2], edg[1]) if edg[0] != 0 else 
+        (edg[0], edg[2], edg[1]) for edg in edg_indices ]
+    edg_indices = np.asarray(edg_indices + reverse_indices)
+    matrix[edg_indices[:,0], edg_indices[:,1], edg_indices[:,2]] = 1
+    # for edg in edges:
+    #     if edg[2] not in graph_keys:
+    #         continue
+    #     edge_type = graph_keys.index(edg[2])
+    #     matrix[edge_type, edg[0], edg[1]] = 1
+    #     if edge_type != 0:
+    #         matrix[edge_type+num_keywords, edg[1], edg[0]] = 1
+    #     else:
+    #         matrix[edge_type, edg[1], edg[0]] = 1
 
     matrix[num_keywords, :, :] = np.identity(num_notes)
     matrix = th.Tensor(matrix)
     return matrix
 
 
-def edges_to_matrix_short(edges, slice_index, graph_keys):
+def edges_to_matrix_short_old(edges, slice_index, graph_keys):
     num_keywords = len(graph_keys)
     num_notes = slice_index[1] - slice_index[0]
     matrix = np.zeros((num_keywords * 2, num_notes, num_notes))
@@ -46,6 +54,26 @@ def edges_to_matrix_short(edges, slice_index, graph_keys):
     matrix[num_keywords, :, :] = np.identity(num_notes)
     matrix = th.Tensor(matrix)
 
+    return matrix
+
+def edges_to_matrix_short(edges, slice_index, graph_keys):
+    num_keywords = len(graph_keys)
+    num_notes = slice_index[1] - slice_index[0]
+    matrix = np.zeros((num_keywords * 2, num_notes, num_notes))
+    start_edge_index = binary_index_for_edge(edges, slice_index[0])
+    end_edge_index = binary_index_for_edge(edges, slice_index[1] + 1)
+    graph_dict = {key: i for i, key in enumerate(graph_keys) }
+
+    edg_indices = [(graph_dict[edg[2]], edg[0]-slice_index[0], edg[1]-slice_index[0])  
+                    for edg in edges[start_edge_index:end_edge_index]
+                    if edg[2] in graph_dict and edg[1] < slice_index[1] ]
+
+    reverse_indices = [ (edg[0]+num_keywords, edg[2], edg[1]) if edg[0] != 0 else 
+        (edg[0], edg[2], edg[1]) for edg in edg_indices ]
+    edg_indices = np.asarray(edg_indices + reverse_indices)
+    matrix[edg_indices[:,0], edg_indices[:,1], edg_indices[:,2]] = 1
+    matrix[num_keywords, :, :] = np.identity(num_notes)
+    matrix = th.Tensor(matrix)
     return matrix
 
 
