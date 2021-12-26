8768458b4bd617e57ed321ea49c4408ad24ecb7c
Dasaem Jeong <dasaemj@sogang.ac.kr>
2021-12-26 12:08:15 +0900

    batchfy residual connector

--- virtuoso/residual_selector.py
+++ virtuoso/residual_selector.py
@@ -30,13 +30,22 @@ class TempoVecMeasSelector(nn.Module):
 
     def forward(self, x, note_locations):
         measure_numbers = note_locations['measure']
-        num_measures = measure_numbers[-1] - measure_numbers[0] + 1
+        max_num_measures = torch.max(measure_numbers - measure_numbers[:,0:1]) + 1
+        qpm_primo = x[:, :, QPM_PRIMO_IDX:QPM_PRIMO_IDX+1]
+        tempo_primo = x[:, :, TEMPO_PRIMO_IDX:]
 
-        qpm_primo = x[:, :, QPM_PRIMO_IDX].view(1, -1, 1)
-        tempo_primo = x[:, :, TEMPO_PRIMO_IDX:].view(1, -1, 2)
-        # beat_tempos = self.note_tempo_infos_to_beat(y, beat_numbers, start_index, QPM_INDEX)
-        beat_qpm_primo = qpm_primo[0, 0, 0].repeat((1, num_measures, 1))
-        beat_tempo_primo = tempo_primo[0, 0, :].repeat((1, num_measures, 1))
+        beat_qpm_primo = qpm_primo[:,0:1].repeat(1, max_num_measures, 1)
+        beat_tempo_primo = tempo_primo[:,0:1].repeat(1,max_num_measures, 1)
         beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], measure_numbers, use_mean=False)
+        return torch.cat([beat_qpm_primo, beat_tempo_primo, beat_tempo_vector], dim=-1)
 
-        return torch.cat((beat_qpm_primo, beat_tempo_primo, beat_tempo_vector), dim=-1)
+        # num_measures = measure_numbers[-1] - measure_numbers[0] + 1
+
+        # qpm_primo = x[:, :, QPM_PRIMO_IDX].view(1, -1, 1)
+        # tempo_primo = x[:, :, TEMPO_PRIMO_IDX:].view(1, -1, 2)
+        # # beat_tempos = self.note_tempo_infos_to_beat(y, beat_numbers, start_index, QPM_INDEX)
+        # beat_qpm_primo = qpm_primo[0, 0, 0].repeat((1, num_measures, 1))
+        # beat_tempo_primo = tempo_primo[0, 0, :].repeat((1, num_measures, 1))
+        # beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], measure_numbers, use_mean=False)
+
+        # return torch.cat((beat_qpm_primo, beat_tempo_primo, beat_tempo_vector), dim=-1)
