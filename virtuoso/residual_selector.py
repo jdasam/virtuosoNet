de3e9fcb74f62d6dcd2b720c9c41b4c4879abeca
Dasaem Jeong <dasaemj@sogang.ac.kr>
2022-01-22 20:09:07 +0900

    modify indentation

--- virtuoso/residual_selector.py
+++ virtuoso/residual_selector.py
@@ -8,55 +8,56 @@ from .utils import note_feature_to_beat_mean, note_tempo_infos_to_beat
 
 
 class TempoVecSelector(nn.Module):
-    def __init__(self):
-        super(TempoVecSelector, self).__init__()
+  def __init__(self):
+    super(TempoVecSelector, self).__init__()
 
-    def forward(self, x, note_locations):
-        measure_numbers = note_locations['measure']
-        max_num_measures = torch.max(measure_numbers - measure_numbers[:,0:1]) + 1
-        qpm_primo = x[:, :, QPM_PRIMO_IDX:QPM_PRIMO_IDX+1]
-        tempo_primo = x[:, :, TEMPO_PRIMO_IDX:]
+  def forward(self, x, note_locations):
+    measure_numbers = note_locations['measure']
+    max_num_measures = torch.max(measure_numbers - measure_numbers[:,0:1]) + 1
+    qpm_primo = x[:, :, QPM_PRIMO_IDX:QPM_PRIMO_IDX+1]
+    tempo_primo = x[:, :, TEMPO_PRIMO_IDX:]
 
-        beat_qpm_primo = qpm_primo[:,0:1].repeat(1, max_num_measures, 1)
-        beat_tempo_primo = tempo_primo[:,0:1].repeat(1,max_num_measures, 1)
-        beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], measure_numbers, use_mean=False)
-        # return torch.cat([beat_qpm_primo, beat_tempo_primo, beat_tempo_vector], dim=-1)
+    beat_qpm_primo = qpm_primo[:,0:1].repeat(1, max_num_measures, 1)
+    beat_tempo_primo = tempo_primo[:,0:1].repeat(1,max_num_measures, 1)
+    beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], measure_numbers, use_mean=False)
+    # return torch.cat([beat_qpm_primo, beat_tempo_primo, beat_tempo_vector], dim=-1)
 
 
-        beat_numbers = note_locations['beat']
-        max_num_beats = torch.max(beat_numbers - beat_numbers[:,0:1]) + 1
+    beat_numbers = note_locations['beat']
+    max_num_beats = torch.max(beat_numbers - beat_numbers[:,0:1]) + 1
 
-        qpm_primo = x[:, :, QPM_PRIMO_IDX:QPM_PRIMO_IDX+1]
-        tempo_primo = x[:, :, TEMPO_PRIMO_IDX:]
-        # beat_tempos = self.note_tempo_infos_to_beat(y, beat_numbers, start_index, QPM_INDEX)
-        beat_qpm_primo = qpm_primo[:, 0:1].repeat((1, max_num_beats, 1))
-        beat_tempo_primo = tempo_primo[:, 0:1].repeat((1, max_num_beats, 1))
-        beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], beat_numbers, use_mean=False)
+    qpm_primo = x[:, :, QPM_PRIMO_IDX:QPM_PRIMO_IDX+1]
+    tempo_primo = x[:, :, TEMPO_PRIMO_IDX:]
+    # beat_tempos = self.note_tempo_infos_to_beat(y, beat_numbers, start_index, QPM_INDEX)
+    beat_qpm_primo = qpm_primo[:, 0:1].repeat((1, max_num_beats, 1))
+    beat_tempo_primo = tempo_primo[:, 0:1].repeat((1, max_num_beats, 1))
+    beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], beat_numbers, use_mean=False)
 
-        return torch.cat((beat_qpm_primo, beat_tempo_primo, beat_tempo_vector), dim=-1)
+    return torch.cat((beat_qpm_primo, beat_tempo_primo, beat_tempo_vector), dim=-1)
 
 class TempoVecMeasSelector(nn.Module):
-    def __init__(self):
-        super(TempoVecMeasSelector, self).__init__()
+  def __init__(self):
+    super(TempoVecMeasSelector, self).__init__()
 
-    def forward(self, x, note_locations):
-        measure_numbers = note_locations['measure']
-        max_num_measures = torch.max(measure_numbers - measure_numbers[:,0:1]) + 1
-        qpm_primo = x[:, :, QPM_PRIMO_IDX:QPM_PRIMO_IDX+1]
-        tempo_primo = x[:, :, TEMPO_PRIMO_IDX:]
+  def forward(self, x, note_locations):
+    measure_numbers = note_locations['measure']
+    max_num_measures = torch.max(measure_numbers - measure_numbers[:,0:1]) + 1
+    qpm_primo = x[:, :, QPM_PRIMO_IDX:QPM_PRIMO_IDX+1]
+    tempo_primo = x[:, :, TEMPO_PRIMO_IDX:]
 
-        beat_qpm_primo = qpm_primo[:,0:1].repeat(1, max_num_measures, 1)
-        beat_tempo_primo = tempo_primo[:,0:1].repeat(1,max_num_measures, 1)
-        beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], measure_numbers, use_mean=False)
-        return torch.cat([beat_qpm_primo, beat_tempo_primo, beat_tempo_vector], dim=-1)
+    beat_qpm_primo = qpm_primo[:,0:1].repeat(1, max_num_measures, 1)
+    beat_tempo_primo = tempo_primo[:,0:1].repeat(1,max_num_measures, 1)
+    beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], measure_numbers, use_mean=False)
 
-        # num_measures = measure_numbers[-1] - measure_numbers[0] + 1
+    return torch.cat([beat_qpm_primo, beat_tempo_primo, beat_tempo_vector], dim=-1)
 
-        # qpm_primo = x[:, :, QPM_PRIMO_IDX].view(1, -1, 1)
-        # tempo_primo = x[:, :, TEMPO_PRIMO_IDX:].view(1, -1, 2)
-        # # beat_tempos = self.note_tempo_infos_to_beat(y, beat_numbers, start_index, QPM_INDEX)
-        # beat_qpm_primo = qpm_primo[0, 0, 0].repeat((1, num_measures, 1))
-        # beat_tempo_primo = tempo_primo[0, 0, :].repeat((1, num_measures, 1))
-        # beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], measure_numbers, use_mean=False)
+    # num_measures = measure_numbers[-1] - measure_numbers[0] + 1
 
-        # return torch.cat((beat_qpm_primo, beat_tempo_primo, beat_tempo_vector), dim=-1)
+    # qpm_primo = x[:, :, QPM_PRIMO_IDX].view(1, -1, 1)
+    # tempo_primo = x[:, :, TEMPO_PRIMO_IDX:].view(1, -1, 2)
+    # # beat_tempos = self.note_tempo_infos_to_beat(y, beat_numbers, start_index, QPM_INDEX)
+    # beat_qpm_primo = qpm_primo[0, 0, 0].repeat((1, num_measures, 1))
+    # beat_tempo_primo = tempo_primo[0, 0, :].repeat((1, num_measures, 1))
+    # beat_tempo_vector = note_feature_to_beat_mean(x[:,:,TEMPO_IDX:TEMPO_IDX+5], measure_numbers, use_mean=False)
+
+    # return torch.cat((beat_qpm_primo, beat_tempo_primo, beat_tempo_vector), dim=-1)
