da63f41b431e10c3e2b18ac1e6b4200ec920c8d9
Dasaem Jeong <dasaemj@sogang.ac.kr>
2022-01-22 20:09:46 +0900

    always get 8 obj from batch

--- virtuoso/emotion.py
+++ virtuoso/emotion.py
@@ -12,7 +12,7 @@ def get_style_from_emotion_data(model, emotion_loader, device):
     for i, batch in enumerate(emotion_loader):
       origin_data = emotion_loader.dataset.data[i*5]
       perform_z_set = {'score_path':origin_data['score_path'], 'perform_path':origin_data['perform_path']}
-      batch_x, batch_y, note_locations, _, _, edges = batch_to_device(batch, device)
+      batch_x, batch_y, _, _, note_locations, _, _, edges = batch_to_device(batch, device)
       perform_z_tensor = model.encode_style(batch_x, batch_y, edges, note_locations)
       perform_z_np_array = perform_z_tensor.detach().cpu().numpy()
       for j in range(5):
